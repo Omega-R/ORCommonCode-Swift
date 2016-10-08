f7b9e64560d3a08ab7685d08d0132b131dba5d5a
Maxim Soloviev <maxim@omega-r.com>
2016-10-08 12:19:04 +0300

    swift: migration to swift 3, pod version was set to 3.0.0 to indicate association with 3rd version of swift

--- Pod/Classes/ORCommon.swift
+++ Pod/Classes/ORCommon.swift
@@ -8,19 +8,19 @@
 
 import Foundation
 
-public func NSLS(key: String) -> String {
+public func NSLS(_ key: String) -> String {
     return NSLocalizedString(key, comment: "")
 }
 
-public func or_typeToString<T>(type: T.Type) -> String {
-    return String(type)
+public func or_typeToString<T>(_ type: T.Type) -> String {
+    return String(describing: type)
 }
 
-public func or_instanceTypeToString(instance: Any) -> String {
-    let s = String(instance.dynamicType).componentsSeparatedByString(".").last!
+public func or_instanceTypeToString(_ instance: Any) -> String {
+    let s = String(describing: type(of: (instance) as AnyObject)).components(separatedBy: ".").last!
     return s
 }
 
-public func or_safeString(str: String?) -> String {
+public func or_safeString(_ str: String?) -> String {
     return str ?? ""
 }
